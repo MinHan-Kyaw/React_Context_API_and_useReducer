d4dfe9093528cac014595152856881987987c4a3
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-06-03 14:26:43 +0700

    Outsourcing Context & State Into a Separate Provider Component

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,16 +1,12 @@
-import { forwardRef, useImperativeHandle, useRef, useContext } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 import Cart from "./Cart";
-import { CartContext } from "../store/shopping-cart-context";
 
 const CartModal = forwardRef(function Modal(
   { cartItems, onUpdateCartItemQuantity, title, actions },
   ref
 ) {
   const dialog = useRef();
-
-  const { items, updateItemQuantity } = useContext(CartContext);
-
   useImperativeHandle(ref, () => {
     return {
       open: () => {
@@ -22,7 +18,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
